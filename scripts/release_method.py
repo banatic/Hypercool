cbbeaf1edc32236f0d9ef24bf503b191dd7f7633
moominda <moominda@naver.com>
2025-11-19 13:55:32 +0900

    Chore : latest.json 수정

--- scripts/release_method.py
+++ scripts/release_method.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":
         raise SystemExit(main())
 
     CONFIG = ReleaseConfig(
-        version="0.1.9",
+        version="0.2.0",
         notes="달력 위젯 개선",
         pub_date=None,  # None 이면 현재 UTC 시간이 사용됩니다.
         skip_build=False,
